5ddf1ae68650b528b75814dc8b7627018884a05b
BerZaak <azzerti147@gmail.com>
2023-11-20 21:41:16 +0100

    Refactored all

--- src/main/java/fr/berzaak/superguilds/SuperGuilds.kt
+++ src/main/java/fr/berzaak/superguilds/SuperGuilds.kt
@@ -1,20 +1,19 @@
 package fr.berzaak.superguilds
 
-import fr.berzaak.superguilds.commands.GuildCreateCommand
-import fr.berzaak.superguilds.managers.GuildManager
+import fr.berzaak.superguilds.commands.GuildCommand
 import fr.berzaak.superguilds.utils.commands.CommandFramework
 import org.bukkit.plugin.java.JavaPlugin
 
 class SuperGuilds : JavaPlugin() {
 
-    val guildManager = GuildManager()
+
 
     override fun onEnable() {
         instance = this
 
         /* Register Commands */
         val commandFramework = CommandFramework(this)
-        commandFramework.registerCommands(GuildCreateCommand())
+        commandFramework.registerCommands(GuildCommand())
 
     }
 
--- src/main/java/fr/berzaak/superguilds/guild/GuildTier.kt
+++
@@ -1,3 +0,0 @@
-package fr.berzaak.superguilds.guild
-
-data class GuildTier(var xpMultiplier: Int, var maxBankPage: Int, var maxMember: Int)
