bf842993feb2e8a2552954d319b3990143a0590d
BerZaak <azzerti147@gmail.com>
2023-11-20 21:03:39 +0100

    New create command

--- src/main/java/fr/berzaak/superguilds/SuperGuilds.kt
+++ src/main/java/fr/berzaak/superguilds/SuperGuilds.kt
@@ -1,16 +1,20 @@
 package fr.berzaak.superguilds
 
+import fr.berzaak.superguilds.commands.GuildCreateCommand
+import fr.berzaak.superguilds.managers.GuildManager
 import fr.berzaak.superguilds.utils.commands.CommandFramework
 import org.bukkit.plugin.java.JavaPlugin
 
 class SuperGuilds : JavaPlugin() {
+
+    val guildManager = GuildManager()
 
     override fun onEnable() {
         instance = this
 
         /* Register Commands */
         val commandFramework = CommandFramework(this)
-
+        commandFramework.registerCommands(GuildCreateCommand())
 
     }
 
@@ -20,6 +24,7 @@ class SuperGuilds : JavaPlugin() {
 
     companion object {
         lateinit var instance: SuperGuilds
+            private set
     }
 
 }
