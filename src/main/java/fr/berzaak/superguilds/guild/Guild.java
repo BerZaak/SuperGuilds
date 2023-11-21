44417896a968251ddafa69ca737ef0bad91db15e
BerZaak <azzerti147@gmail.com>
2023-11-22 00:46:45 +0100

    Tier system

--- src/main/java/fr/berzaak/superguilds/guild/Guild.java
+++ src/main/java/fr/berzaak/superguilds/guild/Guild.java
@@ -7,9 +7,17 @@ import java.util.Set;
 public final class Guild {
 
     private final Set<GuildPlayer> players = Sets.newHashSet();
+    private final GuildTier guildTier;
 
     public Guild(String name, GuildPlayer owner) {
-
+        guildTier = new GuildTier(1);
     }
 
+    public Set<GuildPlayer> getPlayers() {
+        return players;
+    }
+
+    public GuildTier getGuildTier() {
+        return guildTier;
+    }
 }
