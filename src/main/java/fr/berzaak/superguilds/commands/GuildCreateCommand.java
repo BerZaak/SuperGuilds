7f5e39120344e57c542818ca87f95912613f1a4f
BerZaak <azzerti147@gmail.com>
2023-12-02 20:44:52 +0100

    idk

--- src/main/java/fr/berzaak/superguilds/commands/GuildCreateCommand.java
+++ src/main/java/fr/berzaak/superguilds/commands/GuildCreateCommand.java
@@ -2,7 +2,9 @@ package fr.berzaak.superguilds.commands;
 
 import fr.berzaak.superguilds.SuperGuilds;
 import fr.berzaak.superguilds.guild.Guild;
-import fr.berzaak.superguilds.utils.GPlayerUtils;
+import fr.berzaak.superguilds.guild.GuildPlayer;
+import fr.berzaak.superguilds.manager.GuildManager;
+import fr.berzaak.superguilds.manager.PlayerManager;
 import fr.berzaak.superguilds.utils.commands.BCommand;
 import fr.berzaak.superguilds.utils.commands.Command;
 import fr.berzaak.superguilds.utils.commands.CommandArgs;
@@ -10,6 +12,8 @@ import org.bukkit.entity.Player;
 
 public final class GuildCreateCommand implements BCommand {
 
+    private final PlayerManager playerManager = SuperGuilds.getInstance().getPlayerManager();
+    private final GuildManager guildManager = SuperGuilds.getInstance().getGuildManager();
 
     @Override
     @Command(name = "guild.create", inGameOnly = true, description = "Create new guild")
@@ -18,17 +22,22 @@ public final class GuildCreateCommand implements BCommand {
         if (args.length() != 1) return;
 
         Player player = args.getPlayer();
+        GuildPlayer guildPlayer = playerManager.getGuildPlayer(player.getUniqueId());
 
-        if (GPlayerUtils.hasGuild(SuperGuilds.getInstance().getGuildManager().getGuildPlayer(player.getUniqueId()))) {
-            player.sendMessage("§eYou're already in a guild");
-            return;
+        if (guildPlayer.getGuild().isPresent()) {
+
+            player.sendMessage("§cYou're already in a guild !");
+
+        } else {
+
+            Guild guild = new Guild(args.getArgs(0), guildPlayer);
+
+            guildManager.createNewGuild(guild);
+
+            player.sendMessage("§eYou just created the new guild : §6" + args.getArgs(0));
+
         }
 
-        Guild guild = new Guild(args.getArgs(0), SuperGuilds.getInstance().getGuildManager().getGuildPlayer(player.getUniqueId()));
-
-        SuperGuilds.getInstance().getGuildManager().createNewGuild(guild);
-
-        player.sendMessage("§eYou just created the new guild : §6" + args.getArgs(0));
 
     }
 }
