68b6da5d3f29291eeab2c7d611426699df64f9d7
BerZaak <azzerti147@gmail.com>
2023-12-05 15:18:27 +0100

    stuffs

--- src/main/java/fr/berzaak/superguilds/tier/TierXpMultiplier.java
+++ src/main/java/fr/berzaak/superguilds/tier/TierXpMultiplier.java
@@ -19,36 +19,23 @@ public final class TierXpMultiplier extends Tier{
     @Override
     public Double getMultiplier(int level) {
 
-        switch (level) {
-
-            case 1:
-                return 1.5;
-            case 2:
-                return 2.5;
-            case 3:
-                return 3.0;
-            default:
-                return 0.0;
-
-        }
+        return switch (level) {
+            case 1 -> 1.5;
+            case 2 -> 2.5;
+            case 3 -> 3.0;
+            default -> 0.0;
+        };
     }
 
 
     @Override
     public int getPrice(int level) {
 
-        switch (level) {
-
-
-            case 1:
-                return 10000;
-            case 2:
-                return 30000;
-            case 3:
-                return 50000;
-            default:
-                return 0;
-
-        }
+        return switch (level) {
+            case 1 -> 10000;
+            case 2 -> 30000;
+            case 3 -> 50000;
+            default -> 0;
+        };
     }
 }
